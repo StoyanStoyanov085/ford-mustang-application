fd1413fc5bb539c76a9e6be87aa3b56193c5db23
StoyanStoyanov085 <nikolay.stoyanov.kz@mail.bg>
2026-06-20 01:51:53 +0300

    feat: register interceptors

---
+++ src/main/java/com/example/fordmustangapplication/config/WebMvcConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.fordmustangapplication.config;
+
+import com.example.fordmustangapplication.security.SessionCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private final SessionCheckInterceptor interceptor;
+
+    public WebMvcConfiguration(SessionCheckInterceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/img/**");
+    }
+}
